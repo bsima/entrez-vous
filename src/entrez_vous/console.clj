6284cb3800177bd11bf32c3b0b47c2b0b4d279e2
andy c <t2ahc@sad-panda.local>
2013-09-13 00:49:30 -0400

    add name to command line functionality

--- src/entrez_vous/console.clj
+++ src/entrez_vous/console.clj
@@ -7,6 +7,12 @@
   (cli args
        "This program scrapes author abstracts from NCBI Pubmed database."
        ["-h" "--help" "Prints the help banner." :default false :flag true]
+       ["-n" "--name" "Add an author name to target."
+        :assoc-fn (fn [previous key val]
+                    (assoc previous key
+                           (if-let [oldval (get previous key)]
+                             (merge oldval val)
+                             (hash-set val))))]
        ["-e" "--email" "Identifying e-mail address for user."]
        ["-i" "--input" "Read author names from a text file."]
        ["-h" "--hops" "Specifies number of hops to take." :default 0 :parse-fn #(Integer. %)]
--- test/entrez_vous/console_test.clj
+++ test/entrez_vous/console_test.clj
@@ -5,13 +5,16 @@
 (deftest test-parsing
   (testing "That the command line arguments work!"
     (let [[parsed-values _ _]
-          (parse-args ["--email"  "foo@bar.baz"
+          (parse-args ["--name"   "Baron Davis"
+                       "--name"   "Billy Joel"
+                       "--email"  "foo@bar.baz"
                        "--input"  "qux.txt"
                        "--hops"   "10"
                        "--limit"  "11"
                        "--output" "/path/to"
                        "--help"])]
       (is (= (:help parsed-values) true))
+      (is (= (:name parsed-values) #{"Billy Joel" "Baron Davis"}))
       (is (= (:email parsed-values) "foo@bar.baz"))
       (is (= (:hops parsed-values) 10))
       (is (= (:input parsed-values) "qux.txt"))
