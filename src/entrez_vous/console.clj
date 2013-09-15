4625da2e902a38f977c1cebae509644428fd3979
andy c <t2ahc@sad-panda.local>
2013-09-15 10:57:43 -0400

    integrate command line interface and core functions

--- src/entrez_vous/console.clj
+++ src/entrez_vous/console.clj
@@ -1,6 +1,10 @@
 (ns entrez-vous.console
   (:gen-class :main true)
-  (:use [clojure.tools.cli :only [cli]]))
+  (:use [entrez-vous.core :only [retrieve-author-uids
+                                 retrieve-related-uids
+                                 retrieve-abstracts]]
+        [clojure.tools.cli :only [cli]]
+        [clojure.java.io]))
 
 (defn parse-args
   [args]
@@ -13,11 +17,36 @@
                            (if-let [oldval (get previous key)]
                              (merge oldval val)
                              (hash-set val))))]
-       ["-e" "--email" "Identifying e-mail address for user."]
        ["-i" "--input" "Read author names from a text file."]
-       ["-h" "--hops" "Specifies number of hops to take." :default 0 :parse-fn #(Integer. %)]
-       ["-l" "--limit" "Limits number of additional authors." :default 0 :parse-fn #(Integer. %)]
+       ["-h" "--hop" "Hop to related papers." :default false :flag true]
        ["-o" "--output" "Choose an output directory." :default "."]))
+
+(defn scrape-abstracts [author-name filename]
+  (let [abstracts (retrieve-abstracts (retrieve-author-uids author-name))]
+    (Thread/sleep 500)
+    (with-open [wrtr (writer filename)]
+      (doseq [abstract abstracts] (do (.write wrtr abstract)
+                                      (.write wrtr "\n\n"))))))
+
+(defn build-filename [output-directory author-name]
+  (let [clean-author-name (clojure.string/lower-case
+                           (clojure.string/replace author-name " " "-"))]
+    (str output-directory "/" clean-author-name ".txt")))
+
+(defn get-names-from-file [filename]
+  (with-open [rdr (reader filename)]
+    (doall (map clojure.string/trim (line-seq rdr)))))
+
+(defn run-with [options]
+  (let [output-directory (:output options)
+        author-names (into (:name options)
+                           (if-let [filename (:input options)]
+                             (get-names-from-file filename)
+                             nil))]
+    (doseq [author-name author-names]
+      (scrape-abstracts
+       author-name
+       (build-filename output-directory author-name)))))
 
 (defn -main
   [& args]
@@ -25,4 +54,4 @@
     (when (:help options)
       (println banner)
       (System/exit 0))
-    (println options)))
+    (run-with options)))
