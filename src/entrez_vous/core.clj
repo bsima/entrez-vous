69be1e20bf7729c265812123b810055bea4d4d28
andy c <t2ahc@sad-panda.local>
2013-09-14 20:16:19 -0400

    build out required options for using entrez api

--- src/entrez_vous/core.clj
+++ src/entrez_vous/core.clj
@@ -1,17 +1,41 @@
 (ns entrez-vous.core
   (:require [org.httpkit.client :as http]))
 
-(def default-parameters
-  {})
+(def utility-base-url "eutils.ncbi.nlm.nih.gov/entrez/eutils")
 
-(defn clean-name
-  [name]
-  (-> name
-      (clojure.string/replace " " "+")
-      (clojure.string/lower-case)))
+(def utility-webenv (atom nil))
 
-(defn build-query-string
-  [options]
+(def utility-names
+  { :search "esearch.fcgi"
+    :fetch "efetch.fcgi"
+    :link "elink.fcgi"
+    :post "epost.fcgi" })
+
+(def utility-parameters
+  {
+   :all { :tool "entrez-vous-abstract-retriever" :db "pubmed" :usehistory "y" }
+   :fetch { :retmode "xml" :rettype "abstract" :retmax "10000" :retstart "0" }
+   :link { :dbfrom "pubmed" :cmd "neighbor_score" :linkname "pubmed_pubmed" }
+   :search { :retstart "0" :retmax "100000" }
+   :post { }
+   })
+
+;; http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=George+hripcsak[author]
+;; http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=23684593,23975625
+;;
+;; Per session stuff:
+;;
+;; query_key
+;; WebEnv
+;; 
+
+(defn expand-name-for-query [name]
+  (-> name (clojure.string/replace " " "+")
+           (clojure.string/lower-case)
+           (str "[author]")))
+
+(defn build-query-string [parameter-map]
   (let [kw-string (fn [k] (clojure.string/replace (str k) ":" ""))
         kv-string (fn [[k v]] (str (kw-string k) "=" v))]
-    (clojure.string/join "&" (map kv-string options))))
+    (clojure.string/join "&" (map kv-string parameter-map))))
+
--- test/entrez_vous/core_test.clj
+++ test/entrez_vous/core_test.clj
@@ -2,11 +2,13 @@
   (:require [clojure.test :refer :all]
             [entrez-vous.core :refer :all]))
 
-(deftest test-clean-name
+(deftest test-expand-name-for-query
   (testing "Should lower-case and replace whitepace with '+' character."
-    (is (= "herman+miller+aeron" (clean-name "Herman Miller Aeron")))))
+    (is (= "herman+miller+aeron[author]"
+           (expand-name-for-query "Herman Miller Aeron")))))
 
 (deftest test-build-query-string
-  (testing "Joined into a querystring (=,?)."
+  (testing "Should join elements into a querystring with (=,?)."
     (is (= "nom=food&hey=lady"
            (build-query-string [[:nom "food"] [:hey "lady"]])))))
+
