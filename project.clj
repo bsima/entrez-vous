b6ebe759e0585b16c6e27cda59de6a81714d22b8
Ben Sima <bensima@gmail.com>
2014-07-19 11:18:56 -0400

    updated dependency versions - tests still passing

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject entrez-vous "0.1.0-SNAPSHOT"
   :description "Entrez-Vous: A Clojure Project for Fun and Utility!"
-  :url "https://github.com/triposorbust/entrez-vous"
+  :url "https://github.com/bima/entrez-vous"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main entrez-vous.console
   :aot :all
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.cli "0.2.4"]
-                 [http-kit "2.1.10"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [http-kit "2.1.18"]
                  [org.clojure/data.xml "0.0.7"]])
